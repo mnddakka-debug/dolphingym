5d5103bd50e966919e0dfa2c82cb19ebee4b07f7
M Dakka <MOHANNADDAKKA009@GMAIL.COM>
2026-06-08 21:25:52 +0300

    Fix: base path and add Netlify SPA redirects

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: mode === 'production' ? '/dolphingym/' : '/',
+    base: '/',
     build: { minify: false },
     server: {
       port: 3000,
